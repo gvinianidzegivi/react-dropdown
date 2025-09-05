02a014414e61dccb33315e81e7f54ede2af59549
gvinianidzegivi <gvinianidzegivi@gmail.com>
2025-09-05 18:43:31 +0400

    fix: package issue v1.6

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/stories/index.ts'],
   outDir: 'dist',
   format: ['cjs', 'esm'],
   dts: true,
