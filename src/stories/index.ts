582474364858894b145e80a55e310abd12193b3a
gvinianidzegivi <gvinianidzegivi@gmail.com>
2025-09-05 18:33:06 +0400

    fix: package issue v1.4

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,3 +1,3 @@
-// export * from './stories/Dropdown';
+export * from './Dropdown';
 export * from './Badge';
-// export * from './stories/Input';
+export * from './Input';
---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  minify: false,
+  sourcemap: true,
+  splitting: false,
+  target: 'esnext',
+});
